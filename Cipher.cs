0c4781cefdf91467a3ef217c4fbb7fce70cee169
Ynte Veeman <yveeman@outlook.com>
2020-04-01 13:39:38 +0200

    Added SBoxes

--- Cipher.cs
+++ Cipher.cs
@@ -14,6 +14,10 @@ namespace Cryspy
 
         private byte[] lastBlock;
 
+        private int SBoxInputSize = 4;
+        private SBox[] SubBoxes = new SBox[16];
+        private SBox[] SubBoxes1 = { new SBox(0, 2), new SBox(1, 0), new SBox(2, 3), new SBox(3, 1) };
+
 
         public Cipher(String key)
         { /*
@@ -24,6 +28,27 @@ namespace Cryspy
                 */
 
             this.key = BigInteger.Parse(key, System.Globalization.NumberStyles.HexNumber);
+
+            GenerateSubBoxArray();
+        }
+
+        private void GenerateSubBoxArray()
+        {
+            for(byte i = 0; i<16; i++)
+            {
+                byte piece = (byte)((key >> (i * SBoxInputSize)) & 15); 
+
+                while(SubBoxes[piece] != null)
+                {
+                    piece++;
+                    if (piece >= 15) piece -= 15;
+                }
+
+                SubBoxes[piece] = new SBox(i, piece);
+            }
+            
+           /* for (int i = 0; i < 16; i++)
+                Console.WriteLine("{0}: {1} to {2}", i, SubBoxes[i].Input, SubBoxes[i].Output);*/
         }
 
         public byte[] Encrypt(byte[] data)
@@ -49,7 +74,7 @@ namespace Cryspy
                 for (int i = 0; i < block.Length; i++)
                     block[i] = (byte)(block[i] ^ lastBlock[i]);
 
-                encrypted = EncryptBlock(block);
+                encrypted = PermutateBlock(block, true) ;
 
                 lastBlock = (byte[])encrypted.Clone();
 
@@ -81,7 +106,7 @@ namespace Cryspy
             {
                 Array.Copy(bytes, index, block, 0, BlockSize);
 
-                decrypted = DecryptBlock(block);
+                decrypted = PermutateBlock(block, false);
 
                 for (int i = 0; i < decrypted.Length; i++)
                     decrypted[i] = (byte)(decrypted[i] ^ lastBlock[i]);
@@ -96,33 +121,25 @@ namespace Cryspy
             return bytes;
         }
 
-        private byte[] EncryptBlock(byte[] data) 
+        private byte[] PermutateBlock(byte[] data, bool encrypt)
         {
             if (data.Length != BlockSize)
-                throw new Exception("Data Not Block Size");
+                throw new Exception("Data not of block size");
 
+            //Split the data into two 64-bit sections
             long L;
             long R;
 
-            long temp; //Used for swapping L and R and for combining them for the final return
-
-            //Split the data into two 64-bit sections
             L = BitConverter.ToInt64(data.AsSpan(0, 8));
             R = BitConverter.ToInt64(data.AsSpan(8, 8));
 
+            //Perform the Rounds
             for (int i = 0; i < Rounds; i++)
             {
-                long roundKey = Subkey(i);
-
-                R = R ^ roundKey;
-                L = L ^ R;
-
-                if(i < Rounds - 1)
-                {
-                    temp = L;
-                    L = R;
-                    R = temp;
-                }
+                if (encrypt)
+                    EncryptionRound(ref L, ref R, i);
+                else
+                    DecryptionRound(ref L, ref R, i);
             }
 
             //Concatenate the two parts into a single array and return it
@@ -130,44 +147,81 @@ namespace Cryspy
             BitConverter.GetBytes(L).CopyTo(result, 0);
             BitConverter.GetBytes(R).CopyTo(result, 8);
             return result;
-
         }
 
-        private byte[] DecryptBlock(byte[] data)
+        private void EncryptionRound(ref long L, ref long R, int i)
         {
-            if (data.Length != BlockSize)
-                throw new Exception("Data Not Block Size");
+            long roundKey = Subkey(i);
 
-            long L;
-            long R;
+            R = R ^ roundKey;
+            R = Subsistute(R, true);
+            L = L ^ R;
 
-            long temp; //Used for swapping L and R and for combining them for the final return
-
-            //Split the data into two 64-bit sections
-            L = BitConverter.ToInt64(data.AsSpan(0, 8));
-            R = BitConverter.ToInt64(data.AsSpan(8, 8));
-
-            for (int i = Rounds - 1; i >= 0; i--)
+            if (i < Rounds - 1)
             {
-                long roundKey = Subkey(i);
+                long temp = L;
+                L = R;
+                R = temp;
+            }
+        }
 
-                L = L ^ R;
-                R = R ^ roundKey;  
+        private void DecryptionRound(ref long L, ref long R, int i)
+        {
+            i = (Rounds - 1) - i;
 
-                if (i > 0)
+            long roundKey = Subkey(i);
+
+            L = L ^ R;
+            R = Subsistute(R, false);
+            R = R ^ roundKey;
+
+            if (i > 0)
+            {
+                long temp = L;
+                L = R;
+                R = temp;
+            }
+        }
+
+        private long Subsistute(long D, bool forwards)
+        {
+            byte[] bytes = BitConverter.GetBytes(D);
+
+            byte maxPieceValue = (byte)(Math.Pow(2, SBoxInputSize) - 1);
+
+            for (int j = 0; j < bytes.Length; j++)
+            {
+                byte b = bytes[j];
+
+                byte result = 0;
+                byte piece;
+                byte sub = 0;
+
+                for (int i = 0; i < (8 / SBoxInputSize); i++)
                 {
-                    temp = L;
-                    L = R;
-                    R = temp;
+                    piece = (byte)((b >> (i * SBoxInputSize)) & maxPieceValue);
+
+                    foreach (SBox s in SubBoxes)
+                    {
+                        if (forwards && s.Input == piece)
+                        {
+                            sub = s.Output;
+                            break;
+                        }
+                        else if (!forwards && s.Output == piece)
+                        {
+                            sub = s.Input;
+                            break;
+                        }
+                    }
+
+                    result += (byte)(sub << (i * SBoxInputSize));
                 }
+
+                bytes[j] = result;
             }
 
-            //Concatenate the two parts into a single array and return it
-            byte[] result = new byte[BlockSize];
-            BitConverter.GetBytes(L).CopyTo(result, 0);
-            BitConverter.GetBytes(R).CopyTo(result, 8);
-            return result;
-
+            return BitConverter.ToInt64(bytes);
         }
 
         private long Subkey(int i)
@@ -178,6 +232,17 @@ namespace Cryspy
             return BitConverter.ToInt64(longBytes);
         }
 
+
+        private class SBox
+        {
+            public byte Input, Output;
+
+            public SBox(byte input, byte output)
+            {
+                this.Input = input;
+                this.Output = output;
+            }
+        }
    
     }
 }
